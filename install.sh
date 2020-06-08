787f11ab7bcbec8fd470d11d1daeb8bd91790ee6
Ben Davis <bendavis381@gmail.com>
2020-06-08 14:20:45 -0700

    Add mac system preferences

--- install.sh
+++ install.sh
@@ -39,3 +39,39 @@ fi
 echo "--------- Symlinking iTerm2 config"
 ln -s ~/Users/ben/git/mac-config/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 
+echo "--------- Configuring macOS defaults"
+echo "System Preferences > Dock: autohide dock"
+defaults write com.apple.dock autohide -bool true
+
+echo "System Preferences > Dock: size"
+defaults write com.apple.dock tilesize -int 36
+
+echo "System Preferences > Keyboard: set fast key repeat"
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+echo "System Preferences > Keyboard: set fast key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+echo "System Preferences > Trackpad: tap to click"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+echo "System Preferences > Accessibility > Mouse & Trackpad: three finger drag"
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+
+echo "System Preferences > Mission Control: Automatically rearrange Spaces based on most recent use"
+defaults write com.apple.dock mru-spaces -bool false
+
+echo "Finder > Preferences: Show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "Finder > Preferences: Show warning before changing an extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+echo "Finder > View: Show Path Bar"
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Kill affected apps
+for app in "Dock" "Finder"; do
+  killall "${app}" > /dev/null 2>&1
+done
