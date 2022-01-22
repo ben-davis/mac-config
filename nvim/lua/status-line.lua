48e72e4381d11b59e1ae1e77d152b5425736dcae
Ben Davis <ben@bencdavis.com>
2022-01-22 13:42:46 -0500

    Add tabline

--- nvim/lua/status-line.lua
+++ nvim/lua/status-line.lua
@@ -1,4 +1,4 @@
-local gps = require('nvim-gps')
+local gps = require("nvim-gps")
 
 gps.setup()
 
@@ -7,35 +7,42 @@ local symbol = function()
     return gps.get_location()
   end
 
-  return ''
+  return ""
 end
 
 local setup = function()
-  require'lualine'.setup {
+  require "lualine".setup {
     options = {
       icons_enabled = true,
-      theme = 'tokyonight',
-      component_separators = { left = '|', right = '|'},
-      section_separators = { left = '', right = ''},
-      disabled_filetypes = {'NvimTree'}
+      theme = "tokyonight",
+      component_separators = {left = "|", right = "|"},
+      section_separators = {left = "", right = ""},
+      disabled_filetypes = {"NvimTree"}
     },
     sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'branch'},
-      lualine_c = {'filename'},
+      lualine_a = {"mode"},
+      lualine_b = {},
+      lualine_c = {"filename"},
       lualine_x = {},
       lualine_y = {symbol},
-      lualine_z = {'location'}
+      lualine_z = {"location"}
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = {'filename'},
-      lualine_x = {'location'},
+      lualine_c = {"filename"},
+      lualine_x = {"location"},
       lualine_y = {},
       lualine_z = {}
     },
-    tabline = {},
+    tabline = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {"diff"},
+      lualine_y = {"branch"},
+      lualine_z = {"tabs"}
+    },
     extensions = {}
   }
 end
