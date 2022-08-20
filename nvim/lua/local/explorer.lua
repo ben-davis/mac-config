6f52c7999439040e4199a1d926c77dca7c9a5277
Ben Davis <ben@bencdavis.com>
2022-08-20 14:56:33 -0400

    Use trash for file deletion in explorer

--- nvim/lua/local/explorer.lua
+++ nvim/lua/local/explorer.lua
@@ -4,3 +4,16 @@ vim.keymap.set("n", "<Leader>e", ":Neotree toggle .<CR>", { silent = true, norem
 vim.keymap.set("n", "<Leader>E", ":Neotree toggle reveal .<CR>", { silent = true, noremap = true })
 -- vim.keymap.set("n", "<Leader>e", ":NvimTreeToggle<CR>", { silent = true, noremap = true })
 -- vim.keymap.set("n", "<Leader>E", ":NvimTreeFindFile<CR>", { silent = true, noremap = true })
+
+require("neo-tree").setup({
+	filesystem = {
+		commands = {
+			-- Override delete to use trash instead of rm
+			delete = function(state)
+				local path = state.tree:get_node().path
+				vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+                                require("neo-tree.sources.manager").refresh(state.name)
+			end,
+		},
+	},
+})
