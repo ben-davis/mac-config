db7ddd96a46c43679e6656522f3cc3fa6b829502
Ben Davis <ben@bencdavis.com>
2022-09-28 15:00:25 -0400

    Format

--- nvim/lua/local/plugins.lua
+++ nvim/lua/local/plugins.lua
@@ -3,41 +3,41 @@ require("gitsigns").setup()
 
 -- Easier window movement
 require("nvim-window").setup({
-	-- The characters available for hinting windows.
-	chars = {
-		"0",
-		"1",
-		"2",
-		"3",
-		"4",
-		"5",
-		"6",
-		"7",
-		"8",
-		"9",
-		"a",
-		"b",
-		"c",
-		"d",
-		"e",
-		"f",
-		"g",
-		"h",
-		"i",
-		"j",
-		"k",
-		"l",
-		"m",
-		"n",
-		"o",
-	},
+  -- The characters available for hinting windows.
+  chars = {
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+  },
 })
 
 vim.keymap.set("n", "<leader>w", ":lua require('nvim-window').pick()<CR>", { silent = true, noremap = true })
 
 -- Setup autopairs
 require("nvim-autopairs").setup({
-	enable_check_bracket_line = false,
+  enable_check_bracket_line = false,
 })
 
 -- Zen mode
@@ -52,24 +52,26 @@ vim.keymap.set("n", "<leader>m", ":Himalaya<CR>", { silent = true, noremap = tru
 
 -- Floaterm
 local function toggle_floaterm()
-	local cwd = vim.fn.getcwd(0, 0)
-	local project = vim.fn.fnamemodify(cwd, ":t")
+  local cwd = vim.fn.getcwd(0, 0)
+  local project = vim.fn.fnamemodify(cwd, ":t")
 
-	local bufnr = vim.fn["floaterm#terminal#get_bufnr"](project)
+  local bufnr = vim.fn["floaterm#terminal#get_bufnr"](project)
 
-	if bufnr > -1 then
-		vim.api.nvim_command(bufnr .. "FloatermToggle")
-	else
-		vim.api.nvim_command(
-			"FloatermNew --height=0.8 --width=0.9 --wintype=float --name="
-				.. project
-				.. " --title="
-				.. project
-				.. " --position=center"
-		)
-	end
+  if bufnr > -1 then
+    vim.api.nvim_command(bufnr .. "FloatermToggle")
+  else
+    vim.api.nvim_command(
+      "FloatermNew --height=0.8 --width=0.9 --wintype=float --name="
+      .. project
+      .. " --title="
+      .. project
+      .. " --position=center"
+    )
+  end
 end
+
 vim.keymap.set({ "n", "t" }, "<leader>a", toggle_floaterm, { silent = true, noremap = true })
+vim.keymap.set({ "n", "t" }, "<leader>f", ":FloatermToggle<CR>", { silent = true, noremap = true })
 
 -- Git blame popup
 -- The plugin doesn't need setup but I'm adding a comment so I have a reminder. The plugin sets <leader>gm as the map.
@@ -90,8 +92,12 @@ require("rest-nvim").setup({
       json = "jq",
       vnd = "jq",
       html = function(body)
-        return vim.fn.system({"tidy", "-i", "-q", "-"}, body)
+        return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
       end
     }
   }
 })
+
+-- Mind
+require 'window-picker'.setup()
+require 'mind'.setup()
