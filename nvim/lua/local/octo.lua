aac2edcd2b1a46b27b50dfe7ee7f84f2976c1728
Ben Davis <ben@bencdavis.com>
2024-04-30 14:14:48 -0400

    Fix some octo keymaps

--- nvim/lua/local/octo.lua
+++ nvim/lua/local/octo.lua
@@ -1,14 +1,22 @@
 require("octo").setup({
-  mappings = {
-    pull_request = {
-      reload = "gr",
-      open_in_browser = "gb",
-      checkout_pr = "<space>zaazazazaz",
-      merge_pr = "<space>zazazaza",
-      list_commits = "<space>zaajasd",
-      list_changed_files = "<space>zaasfasd",
-    },
-  },
+	mappings = {
+		pull_request = {
+			reload = "gr",
+			open_in_browser = "gb",
+			checkout_pr = "<space>zsdasdjasdaazazazaz",
+			merge_pr = "<space>zazazazhasdasdasdasda",
+			list_commits = "<space>zaajasgiguhanjmd",
+			list_changed_files = "<space>zaasfaasdasd=isd",
+		},
+		review_diff = {
+			select_next_entry = { lhs = "<tab>", desc = "move to previous changed file" },
+			select_prev_entry = { lhs = "<S-tab>", desc = "move to next changed file" },
+		},
+		file_panel = {
+			select_next_entry = { lhs = "<tab>", desc = "move to previous changed file" },
+			select_prev_entry = { lhs = "<S-tab>", desc = "move to next changed file" },
+		},
+	},
 })
 
 vim.keymap.set("n", "<space>go", ":Octo actions<CR>", { noremap = true })
