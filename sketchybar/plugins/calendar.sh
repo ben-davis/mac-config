200dbce79d9398aee8d10487e7bb315767613b9c
Ben Davis <ben@bencdavis.com>
2024-07-13 15:53:47 -0400

    Add "no more events today" to sketchbar clanedar

--- sketchybar/plugins/calendar.sh
+++ sketchybar/plugins/calendar.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 NEXT_EVENT=$(icalBuddy -eed -n -nc -nrd -ea -iep datetime,title -b '' -ps "| @ |" -li 1 eventsToday)
 
+
 if [ "$NEXT_EVENT" == "" ]; then
-  sketchybar --set $NAME drawing=off
+  sketchybar --set $NAME label="No more events today" drawing=on
 else
+  current_time=$(date +%s)
+  IFS="@" read -r summary startTime <<< "$NEXT_EVENT"
+  echo $startTime
+  event_time=$(date -d "$(echo $startTime | sed 's/T/ /;s/ \(.*\)//')" "+%s")
+  time_diff=$(( (event_time - current_time) / 60 ))
+  echo "$summary in $time_diff mins"
+
   sketchybar --set $NAME label="$NEXT_EVENT" drawing=on
 fi
