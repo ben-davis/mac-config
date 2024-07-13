b0c989bb4d032ca95f1c37f7481a3b90a8b87325
Ben Davis <ben@bencdavis.com>
2024-07-13 15:56:15 -0400

    Use case sensitive telescope file search

--- nvim/lua/local/telescope.lua
+++ nvim/lua/local/telescope.lua
@@ -20,7 +20,7 @@ require("telescope").setup({
 			fuzzy = true, -- false will only do exact matching
 			override_generic_sorter = true, -- override the generic sorter
 			override_file_sorter = true, -- override the file sorter
-			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+			case_mode = "respect_case", -- or "ignore_case" or "respect_case"
 		},
 		file_browser = {
 			theme = "ivy",
@@ -48,7 +48,7 @@ require("telescope").setup({
 	},
 })
 
--- require("telescope").load_extension("fzf")
+require("telescope").load_extension("fzf")
 require("telescope").load_extension("projects")
 require("telescope").load_extension("tele_tabby")
 require("telescope").load_extension("dap")
