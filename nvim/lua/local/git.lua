b1f51fb2cf92d3359ded257c69f78abb0156f5e9
Ben Davis <ben@bencdavis.com>
2025-03-04 11:50:46 -0800

    Remove gitlab call

--- nvim/lua/local/git.lua
+++ nvim/lua/local/git.lua
@@ -3,28 +3,28 @@ require("gitsigns").setup()
 
 -- Diffview
 vim.keymap.set(
-	{ "n" },
-	"<leader>gl",
-	":DiffviewFileHistory<CR>",
-	{ silent = true, noremap = true, desc = "Repo history" }
+  { "n" },
+  "<leader>gl",
+  ":DiffviewFileHistory<CR>",
+  { silent = true, noremap = true, desc = "Repo history" }
 )
 vim.keymap.set(
-	{ "n" },
-	"<leader>gh",
-	":DiffviewFileHistory --follow %<CR>",
-	{ silent = true, noremap = true, desc = "File history" }
+  { "n" },
+  "<leader>gh",
+  ":DiffviewFileHistory --follow %<CR>",
+  { silent = true, noremap = true, desc = "File history" }
 )
 vim.keymap.set(
-	{ "v" },
-	"<leader>gh",
-	"<Esc><Cmd>'<,'>DiffviewFileHistory --follow<CR>",
-	{ silent = true, noremap = true, desc = "Range history" }
+  { "v" },
+  "<leader>gh",
+  "<Esc><Cmd>'<,'>DiffviewFileHistory --follow<CR>",
+  { silent = true, noremap = true, desc = "Range history" }
 )
 vim.keymap.set(
-	{ "n" },
-	"<leader>g.",
-	"<Cmd>.DiffviewFileHistory --follow<CR>",
-	{ silent = true, noremap = true, desc = "Line history" }
+  { "n" },
+  "<leader>g.",
+  "<Cmd>.DiffviewFileHistory --follow<CR>",
+  { silent = true, noremap = true, desc = "Line history" }
 )
 vim.keymap.set({ "n" }, "<leader>gd", ":DiffviewOpen <CR>", { silent = true, noremap = true, desc = "Diff worktree" })
 
@@ -36,4 +36,3 @@ vim.g["git_messenger_no_default_mappings"] = true
 vim.keymap.set({ "n" }, "<leader>gm", "<Plug>(git-messenger)", { silent = true, noremap = true, desc = "Git blame" })
 
 require("diffview")
-require("gitlab").setup()
