21ec84b6d56b63713a7cab06a680d5fe4e06b825
Ben Davis <ben@bencdavis.com>
2025-03-17 15:26:50 -0700

    Include config luas

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ vim.api.nvim_command("source ~/.config/nvim/plugins.vim")
 
 require("config.lazy")
 
--- require("local/digraphs")
--- require("local/keymap")
--- require("local/options")
--- require("local/react-native")
+require("local/digraphs")
+require("local/keymap")
+require("local/options")
+require("local/react-native")
