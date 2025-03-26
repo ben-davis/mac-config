de8849e7e7ba9e8985fb6d4ecd8ce45c8a0d89c2
Ben Davis <ben@bencdavis.com>
2025-03-26 12:15:58 -0700

    Add rest.nvim keymap

--- nvim/lua/plugins/rest.lua
+++ nvim/lua/plugins/rest.lua
@@ -7,8 +7,15 @@ return { {
       table.insert(opts.ensure_installed, "http")
     end,
   },
+  config = function()
+    require('rest-nvim').setup({
+      encode_url = true,
+    })
+  end,
+  ft = "http",
   enabled = vim.loop.os_uname().sysname ~= "Linux",
   keys = {
-    { "<leader>R", ":Rest run<CR>", mode = "n" }
+    { "<leader>rr", ":Rest run<CR>",        mode = "n" },
+    { "<leader>re", ":Rest env select<CR>", mode = "n" }
   }
 } }
