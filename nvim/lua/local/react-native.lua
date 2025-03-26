dce33e08a78c93ae42419ab100d4432f7f5d933d
Ben Davis <ben@bencdavis.com>
2025-03-26 12:16:17 -0700

    Remove conflicting binding

--- nvim/lua/local/react-native.lua
+++ nvim/lua/local/react-native.lua
@@ -1,26 +1,26 @@
 local function find_buffer_by_name(name)
-	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		local buf_name = vim.api.nvim_buf_get_name(buf)
-		print("buf", buf_name)
-		if buf_name:match(name .. "$") then
-			return buf
-		end
-	end
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    local buf_name = vim.api.nvim_buf_get_name(buf)
+    print("buf", buf_name)
+    if buf_name:match(name .. "$") then
+      return buf
+    end
+  end
 
-	return -1
+  return -1
 end
 
 local function reload()
-	local app_bufnr = find_buffer_by_name("app%-serve")
+  local app_bufnr = find_buffer_by_name("app%-serve")
 
-	if app_bufnr == -1 then
-		vim.notify("No buffer called app-serve available")
-		return
-	end
+  if app_bufnr == -1 then
+    vim.notify("No buffer called app-serve available")
+    return
+  end
 
-	local terminal_job_id = vim.api.nvim_buf_get_var(app_bufnr, "terminal_job_id")
+  local terminal_job_id = vim.api.nvim_buf_get_var(app_bufnr, "terminal_job_id")
 
-	vim.fn.jobsend(terminal_job_id, "r")
+  vim.fn.jobsend(terminal_job_id, "r")
 end
 
-vim.keymap.set("n", "<Leader>r", reload, { noremap = true, silent = true })
+-- vim.keymap.set("n", "<Leader>r", reload, { noremap = true, silent = true })
