2c79ec10093022238fbe4a7e36e662338458ca65
Ben Davis <bendavis@stripe.com>
2025-09-18 14:31:18 -0700

    Increase formatter timeout

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -7,23 +7,20 @@ return {
           lua = { "stylua" },
           python = { "ruff_format" },
           javascript = { "prettierd", "prettier", stop_after_first = true },
+          javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+          typescript = { "prettierd", "prettier", stop_after_first = true },
+          typescriptreact = { "prettierd", "prettier", stop_after_first = true },
           rust = { "rust" },
           swift = { "swiftformat" },
           sql = { "sqlfmt" },
+          css = { "prettierd", "prettier", stop_after_first = true },
+          yaml = { "prettierd", "prettier", stop_after_first = true },
         },
         format_on_save = {
           -- These options will be passed to conform.format()
-          timeout_ms = 500,
+          timeout_ms = 2000,
           lsp_format = "fallback",
         },
-      })
-
-      -- Sync format on save
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*",
-        callback = function(args)
-          require("conform").format({ bufnr = args.buf })
-        end,
       })
     end,
   },
