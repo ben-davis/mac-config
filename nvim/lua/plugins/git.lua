c7de80e300b66765c204498cf8bbb55a8a25c4a8
Ben Davis <bendavis@stripe.com>
2025-10-21 13:07:23 -0700

    Git changes

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,39 +1,16 @@
 return {
   {
     "sindrets/diffview.nvim",
-    config = function()
-      -- Diffview
-      vim.keymap.set(
-        { "n" },
-        "<leader>gl",
-        ":DiffviewFileHistory<CR>",
-        { silent = true, noremap = true, desc = "Repo history" }
-      )
-      vim.keymap.set(
-        { "n" },
-        "<leader>gh",
-        ":DiffviewFileHistory --follow %<CR>",
-        { silent = true, noremap = true, desc = "File history" }
-      )
-      vim.keymap.set(
-        { "v" },
-        "<leader>gh",
-        "<Esc><Cmd>'<,'>DiffviewFileHistory --follow<CR>",
-        { silent = true, noremap = true, desc = "Range history" }
-      )
-      vim.keymap.set(
-        { "n" },
-        "<leader>g.",
-        "<Cmd>.DiffviewFileHistory --follow<CR>",
-        { silent = true, noremap = true, desc = "Line history" }
-      )
-      vim.keymap.set(
-        { "n" },
-        "<leader>gd",
-        ":DiffviewOpen <CR>",
-        { silent = true, noremap = true, desc = "Diff worktree" }
-      )
-    end,
+    options = {
+      git_cmd = "/usr/local/bin/git-wrapper",
+    },
+    keys = {
+      { "<leader>gl", ":DiffviewFileHistory<CR>", desc = "Repo history" },
+      { "<leader>gh", ":DiffviewFileHistory --follow %<CR>", desc = "File history" },
+      { "<leader>gh", "<Esc><Cmd>'<,'>DiffviewFileHistory --follow<CR>", mode = "v", desc = "Range history" },
+      { "<leader>g.", "<Cmd>.DiffviewFileHistory --follow<CR>", desc = "Line history" },
+      { "<leader>gd", ":DiffviewOpen <CR>", desc = "Diff worktree" },
+    },
   },
   {
     "lewis6991/gitsigns.nvim",
@@ -65,19 +42,19 @@ return {
       })
     end,
   },
-  {
-    "rhysd/git-messenger.vim",
-    config = function()
-      -- Git messenger (blame)
-      vim.g["git_messenger_no_default_mappings"] = true
-      vim.keymap.set(
-        { "n" },
-        "<leader>gm",
-        "<Plug>(git-messenger)",
-        { silent = true, noremap = true, desc = "Git blame" }
-      )
-    end,
-  },
+  -- {
+  --   "rhysd/git-messenger.vim",
+  --   config = function()
+  --     -- Git messenger (blame)
+  --     vim.g["git_messenger_no_default_mappings"] = true
+  --     vim.keymap.set(
+  --       { "n" },
+  --       "<leader>gm",
+  --       "<Plug>(git-messenger)",
+  --       { silent = true, noremap = true, desc = "Git blame" }
+  --     )
+  --   end,
+  -- },
   {
     "akinsho/git-conflict.nvim",
     version = "*",
