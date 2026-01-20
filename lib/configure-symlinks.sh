d4e1962fc593582c62234d42b501eaf784d69474
Ben Davis <ben@bencdavis.com>
2026-01-19 19:34:25 -0800

    Simplify symlinks

--- lib/configure-symlinks.sh
+++ lib/configure-symlinks.sh
@@ -9,7 +9,7 @@ ln -s -f ~/dev/git/mac-config/fish/config.fish ~/.config/fish/config.fish
 ln -s -f ~/dev/git/mac-config/fish/fish_plugins ~/.config/fish/fish_plugins
 
 echo "--------- Symlinking nvim config"
-ln -s -f -n ~/dev/git/mac-config/nvim ~/.config
+ln -sfn ~/dev/git/mac-config/nvim ~/.config
 
 echo "--------- Symlinking bashrc"
 ln -s -f ~/dev/git/mac-config/bashrc ~/.bashrc
@@ -23,21 +23,25 @@ mkdir -p ~/.config/ghostty
 ln -s -f ~/dev/git/mac-config/ghostty.config ~/.config/ghostty/config
 
 echo "--------- Symlinking yabai"
-ln -s -f -n ~/dev/git/mac-config/yabai ~/.config/yabai
+ln -sfn ~/dev/git/mac-config/yabai ~/.config/yabai
 
 echo "--------- Symlinking sketchybar"
-ln -s -f -n ~/dev/git/mac-config/sketchybar ~/.config/sketchybar
+ln -sfn ~/dev/git/mac-config/sketchybar ~/.config/sketchybar
 
 echo "--------- Symlinking skhdrc"
-ln -s -f -n ~/dev/git/mac-config/skhd ~/.config/skhd
+ln -sfn ~/dev/git/mac-config/skhd ~/.config/skhd
 
 if [ -z "$SKIP_SYMLINK_GIT" ]; then
   echo "--------- Symlinking git config"
-  ln -s -f ~/dev/git/mac-config/gitconfig ~/.gitconfig
+  ln -sfn ~/dev/git/mac-config/gitconfig ~/.gitconfig
 fi
 
 echo "--------- Symlinking tmux.conf"
-ln -s -f ~/dev/git/mac-config/tmux.conf ~/.tmux.conf
+ln -sfn ~/dev/git/mac-config/tmux.conf ~/.tmux.conf
 
 echo "--------- Symlinking tmuxinator config"
-ln -s -f -n ~/dev/git/mac-config/tmuxinator ~/.tmuxinator
+ln -sfn ~/dev/git/mac-config/tmuxinator ~/.tmuxinator
+
+echo "--------- Symlinking superfile"
+ln -sfn ~/dev/git/mac-config/superfile ~/.config/superfile
+
